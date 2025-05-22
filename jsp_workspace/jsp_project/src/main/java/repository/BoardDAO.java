7c5339624c33dd0dd6724c9567a1cf9e543657a0
rhdqngo99 <rhdqngo99@naver.com>
2025-05-22 18:23:06 +0900

    Add files via upload

---
+++ jsp_workspace/jsp_project/src/main/java/repository/BoardDAO.java
@@ -0,0 +1,17 @@
+package repository;
+
+import java.util.List;
+
+import domain.BoardVO;
+
+public interface BoardDAO {
+
+	int insert(BoardVO bvo);
+
+	List<BoardVO> getList();
+
+	BoardVO getDetail(int bno);
+
+	int update(BoardVO bvo);
+
+}

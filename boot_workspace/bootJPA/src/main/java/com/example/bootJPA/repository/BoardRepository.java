c7e95172132a0d90df9d68e5ed528c15ed763789
rhdqngo99 <rhdqngo99@naver.com>
2025-06-24 18:32:20 +0900

    Delete boot_workspace/bootJPA/src/main/java/com/example/bootJPA directory

--- boot_workspace/bootJPA/src/main/java/com/example/bootJPA/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bootJPA.repository;
-
-/* JpaRepository<테이블명, id> */
-
-import com.example.bootJPA.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}

b88406f509b46ce152f20fb93f9aeda4f00dcdfa
rhdqngo99 <rhdqngo99@naver.com>
2025-06-27 19:07:53 +0900

    Delete boot_workspace/demo/src/main/java/com/example/demo directory

--- boot_workspace/demo/src/main/java/com/example/demo/repository/CommentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.CommentVO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CommentMapper {
-    int post(CommentVO commentVO);
-}
--- boot_workspace/demo/src/main/java/com/example/demo/service/CommentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.domain.CommentVO;
-
-public interface CommentService {
-    int post(CommentVO commentVO);
-}

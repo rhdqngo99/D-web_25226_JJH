edfaafff214f882448632e0bc0d40d57c370948c
rhdqngo99 <rhdqngo99@naver.com>
2025-06-30 09:58:15 +0900

    Delete boot_workspace/demo/src/main/java/com/example/bootJPA directory

--- boot_workspace/demo/src/main/java/com/example/bootJPA/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bootJPA.repository;
-
-import com.example.bootJPA.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}

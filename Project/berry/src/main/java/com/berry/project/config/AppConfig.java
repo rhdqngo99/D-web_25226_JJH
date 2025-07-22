009d54457146cc86c35c3ff29ff8ec93252e09ae
rhdqngo99 <rhdqngo99@naver.com>
2025-07-22 17:56:25 +0900

    Delete Project/berry/src/main directory

--- Project/berry/src/main/java/com/berry/project/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.berry.project.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:external-api.properties")
-public class AppConfig {
-}
